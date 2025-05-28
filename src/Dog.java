c608ed42b4948c99b2a71d94f1d9f72415a57133
Jhoncgonzalezgs <jhonc.gonzalezg@gmail.com>
2025-05-27 19:45:05 -0500

    https://github.com/Jhoncgonzalezgs/project-java-herencia.git

---
+++ src/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal {
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Dog{");
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
